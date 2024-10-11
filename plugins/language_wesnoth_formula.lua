2a9c569d73b442f3a949268c4818a8cc35bdae6e
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-10-11 21:04:24 +0200

    WIP: 1

--- plugins/language_wesnoth_formula.lua
+++ plugins/language_wesnoth_formula.lua
@@ -2,6 +2,7 @@
 local syntax = require "core.syntax"
 
 -- Language syntax references
+-- https://wiki.wesnoth.org/ReferenceWML#How_WML_works
 -- https://wiki.wesnoth.org/Wesnoth_Formula_Language
 
 -- Real world examples
@@ -10,25 +11,26 @@ local syntax = require "core.syntax"
 syntax.add {
   name = "Wesnoth Formula Language",
   files = { "%.wfl$" },
-  comment = "//",
+  comment = "%#",
   patterns = {
-    { pattern = "//.*",                                           type = "comment"  },
-    { pattern = { "/%*", "%*/" },                                 type = "comment"  },
-    { pattern = { '"', '"', '\\' },                               type = "string"   },
-    { pattern = { "'", "'", '\\' },                               type = "string"   },
+    { pattern = "%#.*",                                           type = "comment"  }, -- 
+    { pattern = { '"', '"', '\\' },                               type = "string"   }, -- 
+    { pattern = { "'", "'", '\\' },                               type = "string"   }, -- 
+    { regex   = "",                               type = "string"   }, -- dice operator
+    
     { pattern = "'\\x%x?%x?%x?%x'",                               type = "string"   }, -- character hexadecimal escape sequence
     { pattern = "'\\u%x%x%x%x'",                                  type = "string"   }, -- character unicode escape sequence
     { pattern = "'\\?.'",                                         type = "string"   }, -- character literal
-    { pattern = "-?0x%x+",                                        type = "number"   },
-    { pattern = "-?%d+[%d%.eE]*f?",                               type = "number"   },
-    { pattern = "-?%.?%d+f?",                                     type = "number"   },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",                           type = "operator" },
-    { pattern = "[%a_][%w_]*%f[(]",                               type = "function" },
-    { regex   = "(?:\\w++\\.?)+(?=\\s+\\w++\\s*\\=\\s*)",         type = "function" }, -- Class name when creating an object
-    { regex   = "[A-Z][A-Z_]+",                                   type = "keyword2" }, -- Constants
+    { pattern = "-?0x%x+",                                        type = "number"   }, -- 
+    { pattern = "-?%d+[%d%.eE]*f?",                               type = "number"   }, -- 
+    { pattern = "-?%.?%d+f?",                                     type = "number"   }, -- 
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",                           type = "operator" }, -- 
+    { pattern = "[%a_][%w_]*%f[(]",                               type = "function" }, -- Function
     { pattern = "[%a_][%w_]*",                                    type = "symbol"   }, -- Symbols
   },
   symbols = {
+    ["where"]     = "keyword2",
+    [""]     = "keyword2",
     [""]     = "keyword2"
   }
 }
